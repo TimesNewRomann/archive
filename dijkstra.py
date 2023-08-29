3e40443cbac5092ec2b6e29dbea524ac09f96685
Seonwoo An <answ1000@gmail.com>
2023-08-29 12:32:04 +0900

    08.29 임시 그래프_성공

--- dijkstra.py
+++ dijkstra.py
@@ -1,43 +1,69 @@
-# 여러개의 노드가 필요함
-# 음의 간선(edge)이 존재하지 않는이상 즉, edge >= 0 양수일때, 동작
+import heapq
 
-# 1. 출발노드 설정
-# 2. 최단 거리 테이블 초기화(최단거리를 기록할 테이블을 정의해야 함)
-# 3. 방문하지 않은 노드 중에서 최단 거리가 가장 짧은 노드를 선택(방문하지 않은 노드를 체크해야 하므로 이를 위한 테이블을 정의해야 함)
-# 4. 해당 노트를 거쳐 다른 노드로 가는 간선 비용을 계산하여 최단 거리 테이블을 업테이트
-# 5. 3~4번 반복
+def dijkstra(graph, start, end):
+    # 그래프의 모든 노드에 대해 초기값을 무한대로 설정하고 시작 노드는 0으로 설정
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
 
-# 최단 거리를 기록할 테이블(리스트) 정의하기
+    # 각 노드까지의 최단 경로를 저장하는 딕셔너리
+    previous_nodes = {}
 
-# 시간복잡도를 고려하여 우선순위 큐(heap)을 사용하여 개선된 다익스트라 알고리즘으로 구현하기
+    # 우선순위 큐를 사용하여 방문할 노드 관리
+    priority_queue = [(0, start)]
 
-import heapq  # 우선순위 큐 구현
-
-
-def dijkstra(graph, start):
-    distances = {node: float('inf')
-                 for node in graph}  # start로 부터의 거리 값을 저장하기 위함. 모든 노드에 대해 최기값을 무한대로 설정
-    distances[start] = 0  # 시작 값은 0
-
-    priority_queue = [(0, start)]  # 우선순위 큐 방문노드 관리
     while priority_queue:
-        current_distance, current_node = heapq.heappop(
-            priority_queue)  # 탐색할 노드, 거리를 가져옴
+        current_distance, current_node = heapq.heappop(priority_queue)
 
-        if current_distance > distances[current_node]:  # 이전 거리보다 크다면 무시
+        # 현재 노드가 이미 더 짧은 경로로 방문된 경우 건너뛴다
+        if current_distance > distances[current_node]:
             continue
 
-        for new_destination, new_distance in graph[current_node].items():
-            distance = current_distance + new_distance  # 해당 노드를 거쳐 갈 때 거리
+        # 현재 노드에 인접한 노드들을 탐색한다
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
 
-            if distance < distance[new_destination]:  # 알고있는 거리보다 작으면 업데이트
-                distances[new_destination] = distance
-                # 다음 인접 거리를 계산하기 위해 큐에 삽입
-                heapq.heappush(priority_queue, (distance, new_destination))
-    return distances
+            # 새로 계산된 거리가 기존의 거리보다 짧은 경우 업데이트한다.
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                previous_nodes[neighbor] = current_node
+                heapq.heappush(priority_queue, (distance, neighbor))
 
+    # 경로 추적
+    path = []
+    current = end
+    while current in previous_nodes:
+        path.insert(0, current)
+        current = previous_nodes[current]
+    path.insert(0, start)
 
-graph = {}
+    return path
 
-result = dijkstra(graph, 'A')
-print(result)
+# 그래프와 시작 노드, 목적지 노드 설정
+graph = {
+    '154': {'55': 1},
+    '55': {'129': 1},
+    '129': {'61': 1},
+    '61': {'135': 1},
+    '135': {'139': 1},
+    '139': {'144': 1},
+    '144': {'16': 2, '17' : 1},
+    '16': {},
+    '17': {'203': 1},
+    '203': {'204': 1},
+    '204': {'205': 1},
+    '205': {'39': 1},
+    '39': {'216': 1},
+    '216': {'147': 1},
+    '147': {'149': 1},
+    '149': {'151': 1},
+    '151': {'153': 1},
+    '153': {}
+}
+start_node = '17' # 시작노드. 외부에서 실시간으로 받아온 현재 위치(노드)를 넣으면, 현재 위치부터 목적노드까지의 경로를 계속 생성해 줄 수 있다.
+end_node = '153' # 목표 노드
+
+# 최단 경로 찾기
+shortest_path = dijkstra(graph, start_node, end_node)
+print("Shortest path:", shortest_path) # 전체 경로 리스트
+print("Oncoming_node:", shortest_path[1:4]) # 앞 3노드
+#포인트스템프드 형식에 넣어서 데이터를 퍼블리시하는 코드 구현해보기
